a31a0d958b863ece7f1cfd2499bcb78c02548ad3
liamrahav <liorogamerdvd@gmail.com>
2016-09-29 06:29:16 -0400

    added QueryItemRepresentable and made Tile conform to it

---
+++ MessagesExtension/QueryItemRepresentable.swift
@@ -0,0 +1,16 @@
+/*
+ Copyright (C) 2016 Apple Inc. All Rights Reserved.
+ See LICENSE.txt for this sample’s licensing information
+ 
+ */
+
+import Foundation
+
+/**
+ Types that conform to the QueryItemRepresentable protocol must implement properties that allow it to be saved as a query item in a URL.
+ */
+protocol QueryItemRepresentable {
+//    var queryItem: URLQueryItem { get }
+    
+    static var queryItemKey: String { get }
+}
--- MessagesExtension/Tile.swift
+++ MessagesExtension/Tile.swift
@@ -11,11 +11,23 @@ import Foundation
 /**
  This enum represents the different states that each indivudal tile within a battleground can have.
  */
-enum Tile {
+enum Tile: QueryItemRepresentable {
     /// The x case refers to when the X player has claimed this tile
     case x
     /// The p case refers to when the O player has claimed this tile
     case o
     /// The empty case refers to when neither player has claimed this tile
     case empty
+    
+    static let all: [Tile] = [.x, .o, .empty]
+    
+    static var queryItemKey: String {
+        return "Tile"
+    }
+}
+
+extension QueryItemRepresentable where Self: Tile {
+    var queryItem: URLQueryItem {
+        return URLQueryItem(name: Self.queryItemKey, value: rawValue)
+    }
 }
